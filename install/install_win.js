20b80d55fe72078620ebfde473f9c0d0ca2a8d94
Tho Phan <tho.phan@newoceaninfosys.com>
2020-11-20 21:28:13 +0700

    Fix install on yarn

--- install/install_win.js
+++ install/install_win.js
@@ -21,49 +21,9 @@ module.exports = function(){
 		}
 	}
 
-	var pattern = new RegExp(".*edge.*", "gi");
-	var choices;
-
-	try {
-		choices = fs.readdirSync(__dirname + "/../../");
-	} catch (error) {}
-
-	if (choices) {
-		choices = choices.filter(value => {
-			return pattern.test(value);
-		});
-
-		choices.push('Not listed');
-
-		inquirer.prompt([{
-			type: 'list',
-			name: 'package',
-			message: 'Which package do you want to use?',
-			choices
-		}])
-		.then(answer => {
-			if (answer.package === 'Not listed') {
-				return manuallyInsert();
-			}
-
-			makeEnv(answer.package);
-		});
-	} else {
-		manuallyInsert();
-	}
-
+	makeEnv('electron-edge-js');
 };
 
 function makeEnv(pack) {
 	fs.writeFileSync(fs.realpathSync(__dirname + '\\..') + "\\.env", `NNP_PACKAGE=${pack}`);
-}
-
-function manuallyInsert() {
-	inquirer.prompt([{
-		type: 'input',
-		name: 'package',
-		message: "Type the name of the package you want to use:\n"
-	}]).then(answer => {
-		makeEnv(answer.package)
-	});
 }
